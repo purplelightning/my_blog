ede966e18b119ac0c261a4daa25ac06ceaf4fca2
wind <958031539@qq.com>
2017-03-15 19:38:04 +0800

    django tutorial init

---
+++ article/templatetags/__init__.py
@@ -0,0 +1,3 @@
+#coding=utf-8
+__author__ = 'ss'
+__date__ = '17-3-14 下午8:38'
---
+++ article/views.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+from django.http import Http404
+from markdown import markdown
+
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
+# Create your views here.
+def home(request):
+    posts=Article.objects.all()
+    paginator=Paginator(posts,5)#每页显示两个
+    page=request.GET.get('page')
+    try:
+        post_list=paginator.page(page)
+    except PageNotAnInteger:
+        post_list=paginator.page(1)
+    except EmptyPage:
+        post_list=paginator.paginator(paginator.num_pages)
+    return render(request, 'home.html', {'post_list' : post_list})
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
+
+def archives(request):
+    try:
+        post_list=Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request,'archives.html',{
+        'post_list':post_list,
+        'error':False,
+    })
+
+def about_me(request):
+    return render(request,'about.html')
+
+def search_tag(request,tag):
+    try:
+        post_list=Article.objects.filter(category__iexact=tag)#contains
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request,'tag.html',{'post_list':post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s=request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list=Article.objects.filter(title__icontains=s)
+            if len(post_list)==0:
+                return render(request,'archives.html',{'post_list':post_list,'error':True})
+            else:
+                return render(request,'archives.html',{'post_list':post_list,'error':False})
+    return redirect('/')
+
+
+
+
+
+
+
+
